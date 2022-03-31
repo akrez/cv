d1509fddd3b19f8efb2d1904ff8bd4552123be4f
ordikhan <mo.ordikhani@gmail.com>
2022-03-31 23:52:32 +0430

    rivise css Adrres

--- assets/PersonalAsset.php
+++ assets/PersonalAsset.php
@@ -9,24 +9,25 @@ class PersonalAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
+        'theme/Personal/vendor/bootstrap/css/bootstrap.min.css',
         'theme/Personal/vendor/icofont/icofont.min.css',
+        'theme/Personal/vendor/remixicon/remixicon.css',
+        'theme/Personal/vendor/owl.carousel/assets/owl.carousel.min.css',
         'theme/Personal/vendor/boxicons/css/boxicons.min.css',
         'theme/Personal/vendor/venobox/venobox.css',
-        'theme/Personal/vendor/owl.carousel/assets/owl.carousel.min.css',
-        'theme/Personal/vendor/aos/aos.css',
         'theme/Personal/css/style.css',
     ];
     public $js = [
-        'theme/Personal/vendor/jquery.easing/jquery.easing.min.js',
-        'theme/Personal/vendor/php-email-form/validate.js',
-        'theme/Personal/vendor/waypoints/jquery.waypoints.min.js',
-        'theme/Personal/vendor/counterup/counterup.min.js',
-        'theme/Personal/vendor/isotope-layout/isotope.pkgd.min.js',
-        'theme/Personal/vendor/venobox/venobox.min.js',
-        'theme/Personal/vendor/owl.carousel/owl.carousel.min.js',
-        'theme/Personal/vendor/typed.js/typed.min.js',
-        'theme/Personal/vendor/aos/aos.js',
-        'theme/Personal/js/main.js',
+        'views\Personal.php/vendor/jquery/jquery.min.js',
+        'views\Personal.php/vendor/bootstrap/js/bootstrap.bundle.min.js',
+        'views\Personal.php/vendor/jquery.easing/jquery.easing.min.js',
+        'views\Personal.php/vendor/php-email-form/validate.js',
+        'views\Personal.php/vendor/waypoints/jquery.waypoints.min.js',
+        'views\Personal.php/vendor/counterup/counterup.min.js',
+        'views\Personal.php/vendor/owl.carousel/owl.carousel.min.js',
+        'views\Personal.php/vendor/isotope-layout/isotope.pkgd.min.js',
+        'views\Personal.php/vendor/venobox/venobox.min.js',
+        'views\Personal.php/js/main.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
